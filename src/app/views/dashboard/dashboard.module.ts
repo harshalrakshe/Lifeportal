9bad84302deceaf98502abaa8cf2bb7e84b9b4d1
Harshal Rakshe <harshalrakshe32@gmail.com>
2023-04-17 13:11:15 +0530

    init

---
+++ src/app/views/dashboard/dashboard.module.ts
@@ -0,0 +1,30 @@
+import { SharedMaterialModule } from "app/shared/shared-material.module";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { NgChartsModule } from "ng2-charts";
+import { NgxEchartsModule } from "ngx-echarts";
+import { SharedPipesModule } from "app/shared/pipes/shared-pipes.module";
+import { DashboardRoutes } from "./dashboard.routing";
+import { AnalyticsComponent } from "./analytics/analytics.component";
+import { MatDialogModule } from "@angular/material/dialog";
+
+@NgModule({
+  imports: [
+    MatDialogModule,
+    CommonModule,
+    SharedMaterialModule,
+    FlexLayoutModule,
+    NgChartsModule,
+
+    NgxEchartsModule.forRoot({
+      echarts: () => import("echarts"),
+    }),
+    SharedPipesModule,
+    RouterModule.forChild(DashboardRoutes),
+  ],
+  declarations: [AnalyticsComponent],
+  exports: [MatDialogModule],
+})
+export class DashboardModule {}
